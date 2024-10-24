3bcd27a5e277afc3e74b6f8ea0835886747aea89
EmmaAli1604 <emma_jimenez1601@ciencias.unam.mx>
2024-10-23 21:20:28 -0600

    navegación con fragments

--- Tarea2.0/app/src/main/java/com/example/tarea20/MainActivity.kt
+++ Tarea2.0/app/src/main/java/com/example/tarea20/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.tarea20
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.navigation.findNavController
+import com.example.tarea20.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+    private lateinit var formsbutton : FloatingActionButton;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragmente(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.mylibrary -> replaceFragmente(LibraryFragment())
+                R.id.pending -> replaceFragmente(PendingBooksFragment())
+                R.id.todayreading -> replaceFragmente(HomeFragment())
+                else -> {}
+            }
+            true
+        }
+
+        binding.buttonadd.setOnClickListener{
+            replaceFragmente(FormsFragment())
         }
     }
+
+    private fun replaceFragmente(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)  // Usamos replace en lugar de setTransition
+        fragmentTransaction.commit()  // Confirmamos la transacción
+    }
+
 }
