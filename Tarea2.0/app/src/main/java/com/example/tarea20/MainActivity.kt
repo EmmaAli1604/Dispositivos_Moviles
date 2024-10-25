487527c153645a744b545c1adbb5eb0c602897ec
EmmaAli1604 <emma_jimenez1601@ciencias.unam.mx>
2024-10-24 19:22:42 -0600

    navigation bottom ready

--- Tarea2.0/app/src/main/java/com/example/tarea20/MainActivity.kt
+++ Tarea2.0/app/src/main/java/com/example/tarea20/MainActivity.kt
@@ -1,54 +1,45 @@
 package com.example.tarea20
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import androidx.navigation.findNavController
 import com.example.tarea20.databinding.ActivityMainBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding;
-    private lateinit var formsbutton : FloatingActionButton;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
-        replaceFragmente(HomeFragment())
+        replaceFragment(HomeFragment())
 
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.mylibrary -> replaceFragmente(LibraryFragment())
-                R.id.pending -> replaceFragmente(PendingBooksFragment())
-                R.id.todayreading -> replaceFragmente(HomeFragment())
+                R.id.mylibrary -> replaceFragment(LibraryFragment())
+                R.id.pending -> replaceFragment(PendingBooksFragment())
+                R.id.todayreading -> replaceFragment(HomeFragment())
+                R.id.usuario -> replaceFragment(UserFragment())
                 else -> {}
             }
             true
         }
 
         binding.buttonadd.setOnClickListener{
-            replaceFragmente(FormsFragment())
+            replaceFragment(FormsFragment())
         }
+
     }
 
-    private fun replaceFragmente(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)  // Usamos replace en lugar de setTransition
-        fragmentTransaction.commit()  // Confirmamos la transacción
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
+
 
 }
